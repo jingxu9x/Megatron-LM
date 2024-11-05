12ded8e594bce15dd0444b1936283d675300784f
jingxu <jingxu@lepton.ai>
2024-11-05 18:39:11 +0800

    feat(bench): support bench gpt model

---
+++ bench_gpt3_single.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Runs the "175B" parameter model
+
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+
+GPUS_PER_NODE=1
+# Change for multinode config
+MASTER_ADDR=localhost
+MASTER_PORT=6000
+NUM_NODES=1
+NODE_RANK=0
+WORLD_SIZE=$(($GPUS_PER_NODE*$NUM_NODES))
+
+
+variant=$(dirname $0)
+WORKDIR=checkpoints
+CHECKPOINT_PATH=$WORKDIR/ckpt/$variant
+TENSORBOARD_LOGS_PATH=$WORKDIR/tensorboard/$variant
+DATA_PATH="" # fake data
+
+DISTRIBUTED_ARGS=(
+    --nproc_per_node $GPUS_PER_NODE 
+    --nnodes $NUM_NODES 
+    --master_addr $MASTER_ADDR 
+    --master_port $MASTER_PORT
+)
+
+GPT_MODEL_ARGS=(
+    --num-layers 32 
+    --hidden-size 2560 
+    --num-attention-heads 20 
+    --seq-length 2048 
+    --max-position-embeddings 2048 
+)
+
+TRAINING_ARGS=(
+    --micro-batch-size 1 
+    --global-batch-size 64 
+    --train-iters 500000 
+    --weight-decay 0.1 
+    --adam-beta1 0.9 
+    --adam-beta2 0.95 
+    --init-method-std 0.006 
+    --clip-grad 1.0 
+    --bf16
+    --lr 6.0e-5 
+    --lr-decay-style cosine 
+    --min-lr 6.0e-6
+    --lr-warmup-fraction .001 
+    --lr-decay-iters 430000 
+)
+
+MODEL_PARALLEL_ARGS=(
+	--tensor-model-parallel-size 1 
+	--pipeline-model-parallel-size 1 
+
+    # mask below if install apex, TE
+    --transformer-impl local
+    # --no-gradient-accumulation-fusion
+)
+
+DATA_ARGS=(
+    --data-path $DATA_PATH 
+    --vocab-size 10000
+    --tokenizer-type NullTokenizer
+    --split 949,50,1
+)
+
+EVAL_AND_LOGGING_ARGS=(
+    --log-interval 1
+    --eval-interval 1000
+    --eval-iters 10
+    --tensorboard-dir $TENSORBOARD_LOGS_PATH 
+)
+
+torchrun ${DISTRIBUTED_ARGS[@]} bench_gpt.py \
+    ${GPT_MODEL_ARGS[@]} \
+    ${TRAINING_ARGS[@]} \
+    ${MODEL_PARALLEL_ARGS[@]} \
+    ${DATA_ARGS[@]} \
+    ${EVAL_AND_LOGGING_ARGS[@]}
