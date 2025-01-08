03e4eb729b7756524caa034fa96e1ea0562cc1f4
jingxu <zhongjie007@yeah.net>
2025-01-08 15:26:41 +0000

    fix(script): bench script and env.yaml

--- bench_gpt3_single.sh
+++ bench_gpt3_single.sh
@@ -4,7 +4,7 @@
 
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
-GPUS_PER_NODE=1
+GPUS_PER_NODE=2
 # Change for multinode config
 MASTER_ADDR=localhost
 MASTER_PORT=6000
@@ -20,40 +20,40 @@ TENSORBOARD_LOGS_PATH=$WORKDIR/tensorboard/$variant
 DATA_PATH="" # fake data
 
 DISTRIBUTED_ARGS=(
-    --nproc_per_node $GPUS_PER_NODE 
-    --nnodes $NUM_NODES 
-    --master_addr $MASTER_ADDR 
+    --nproc_per_node $GPUS_PER_NODE
+    --nnodes $NUM_NODES
+    --master_addr $MASTER_ADDR
     --master_port $MASTER_PORT
 )
 
 GPT_MODEL_ARGS=(
-    --num-layers 12 
-    --hidden-size 2560 
-    --num-attention-heads 20 
-    --seq-length 1024 
-    --max-position-embeddings 1024 
+    --num-layers 32
+    --hidden-size 4096
+    --num-attention-heads 32
+    --seq-length 1024
+    --max-position-embeddings 2048
 )
 
 TRAINING_ARGS=(
-    --micro-batch-size 1 
-    --global-batch-size 64 
-    --train-iters 500000 
-    --weight-decay 0.1 
-    --adam-beta1 0.9 
-    --adam-beta2 0.95 
-    --init-method-std 0.006 
-    --clip-grad 1.0 
+    --micro-batch-size 1
+    --global-batch-size 64
+    --train-iters 500000
+    --weight-decay 0.1
+    --adam-beta1 0.9
+    --adam-beta2 0.95
+    --init-method-std 0.006
+    --clip-grad 1.0
     --bf16
-    --lr 6.0e-5 
-    --lr-decay-style cosine 
+    --lr 6.0e-5
+    --lr-decay-style cosine
     --min-lr 6.0e-6
-    --lr-warmup-fraction .001 
-    --lr-decay-iters 430000 
+    --lr-warmup-fraction .001
+    --lr-decay-iters 430000
 )
 
 MODEL_PARALLEL_ARGS=(
-	--tensor-model-parallel-size 1 
-	--pipeline-model-parallel-size 1 
+	--tensor-model-parallel-size 2
+	--pipeline-model-parallel-size 1
 
     # mask below if install apex, TE
     # --transformer-impl local
@@ -61,7 +61,7 @@ MODEL_PARALLEL_ARGS=(
 )
 
 DATA_ARGS=(
-    --data-path $DATA_PATH 
+    --data-path $DATA_PATH
     --vocab-size 10000
     --tokenizer-type NullTokenizer
     --split 949,50,1
@@ -72,7 +72,7 @@ EVAL_AND_LOGGING_ARGS=(
     --log-throughput
     --eval-interval 1000
     --eval-iters 10
-    --tensorboard-dir $TENSORBOARD_LOGS_PATH 
+    --tensorboard-dir $TENSORBOARD_LOGS_PATH
 )
 
 torchrun ${DISTRIBUTED_ARGS[@]} bench_gpt.py \
